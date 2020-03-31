3319999f8e93d3691354bc75adafd25d39b731bd
sahiljain11 <sahilbot1@gmail.com>
2020-03-31 01:57:05 -0500

    Can save video now! MySQL implementation also added if wanted ^_^

--- javascript/testing_comm/server.py
+++ javascript/testing_comm/server.py
@@ -2,8 +2,13 @@
 
 from flask import Flask, jsonify, request, render_template
 import requests as req
+import subprocess
+import mysql.connector
+import os
+import random
 
 app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = 'storage/'
 
 @app.route('/RL', methods=['GET', 'POST'])
 def learning():
@@ -33,16 +38,42 @@ def learning():
 @app.route('/finish', methods=['POST'])
 def finish():
     print('finish method on python server')
-    # Get and store the video file recieved
+    # Store the video file recieved
     if request.method == 'POST':
-        print('url')
-        print(request.get_data())
+
+        mydb = mysql.connector.connect(
+            host="localhost",
+            db="robotaxi",
+            user="root",
+            password="Mturk$35@"
+        )
+
+        print('Connected to database')
+
+        cursor = mydb.cursor()
+        sql_insert_blob_query = """ INSERT INTO player (player_id, video) VALUES (%s, %s)"""
+
+        player_id = random.randint(10000, 99999)
+        video_file = request.files['video-blob']
+        print(os.path.join(app.config['UPLOAD_FOLDER'], 'test.webm'))
+        video_file.save(os.path.join(app.config['UPLOAD_FOLDER'], 'test.webm'))
+        print('Got video file!')
+        
+        #insert_blob_tuple = (player_id, video_file)
+        #result = cursor.execute(sql_insert_blob_query, insert_blob_tuple)
+        #mydb.commit()
+        #print("Image and file inserted successfully as a BLOB into player")
+
+        if mydb.is_connected():
+            cursor.close()
+            mydb.close()
+            print("Connection to database closed")
         
         return 'finished?'
     else:
         return "ERROR: Only POST requests allowed for /finish"
-            
 
+    
 @app.route('/', methods=['GET', 'POST'])
 def page():
 
